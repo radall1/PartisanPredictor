aa5b71d8beb74cb7281ea3ba885b3bd27cb9ef2a
Mohamed <94929440+radall1@users.noreply.github.com>
2024-04-23 05:42:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,17 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
-with open('/Users/mohamed/Downloads/label_encoder.pkl', 'rb') as f:
+with open('/label_encoder.pkl', 'rb') as f:
     label_encoder = pickle.load(f)
-with open('Users/mohamed/Downloads/tokenizer.json', 'r') as f:
+with open('tokenizer.json', 'r') as f:
         tokenizer_config = f.read()
 tokenizer = tokenizer_from_json(tokenizer_config)
     
 max_sequence_length = 100 # Adjust according to your model's input shape
-model_path = '/Users/mohamed/Downloads/political_model_v2.h5'
+model_path = 'political_model_v2.h5'
 model = tf.keras.models.load_model(model_path)
 
-def predict(text):
-    if tokenizer is None or model is None:
-        raise ValueError("Tokenizer or model is not loaded. Please call load_model_and_tokenizer first.")
-    
+def predict(text): 
     sequence = tokenizer.texts_to_sequences([text])
     padded_sequence = pad_sequences(sequence, maxlen=max_sequence_length)
     predicted_probabilities = model.predict(padded_sequence)
